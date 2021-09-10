f93931534dc7d16cc6c56b013d81751c87e3a32d
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-09-10 09:01:30 +0300

    added local storage saver

--- src/core/store.js
+++ src/core/store.js
@@ -1,7 +1,17 @@
 import { createStore } from "redux";
 import reducers from "~/reducers";
+import { loadState, saveState } from "./storage";
 
-export default createStore(
+const persistedState = loadState();
+
+const store = createStore(
 	reducers,
+	persistedState,
 	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+store.subscribe(() => {
+	saveState(store.getState());
+});
+
+export default store;
